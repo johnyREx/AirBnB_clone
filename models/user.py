6de519ed60d40f7c03bff80bd9b7cb05e33c7cad
johnyREx <johnkwofie99@icloud.com>
2023-10-13 15:06:01 -0700

    models engine filestoarge

--- models/user.py
+++ models/user.py
@@ -7,7 +7,15 @@ from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    """This class inherits from BaseModel and defines a user"""
+    """This class inherits from BaseModel and defines a user.
+
+    Attributes:
+        email (str): the email of the user.
+        password (str): the password of the user.
+        first_name (str): the first name of the user.
+        last_name (str): the last name of the user.
+    """
+
     email = ""
     password = ""
     first_name = ""
