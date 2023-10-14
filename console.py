8c20171fdbfac3497647174a5d297f3ec9cc9008
Haffie023 <joyce.muchoki@gmail.com>
2023-10-14 12:59:03 -0700

    console

--- console.py
+++ console.py
@@ -1,36 +1,20 @@
-#!/usr/bin/python3
-"""
-This module defines the entry point of the command interpreter.
-"""
-
+#!/usr/bin/env python3
+"""Console module"""
 import cmd
 
-
 class HBNBCommand(cmd.Cmd):
-    """
-    This class defines the command interpreter.
-    """
-    prompt = "(hbnb)"
+    """HBNBCommand class"""
+    
+    prompt = "(hbnb) "
 
     def do_quit(self, arg):
-        """
-        Quit command to exit the program.
-        """
         return True
 
     def do_EOF(self, arg):
-        """
-        EOF command to exit the program.
-        """
         print()
         return True
-
-    def emptyline(self):
-        """
-        Empty line + ENTER shouldn't execute anything.
-        """
-        pass
 
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
+
