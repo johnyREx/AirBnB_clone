6ad5e1225fdb6fdc7dcc390916446a3145a99bbd
johnyREx <johnkwofie99@icloud.com>
2023-10-15 11:59:58 -0700

    console py stress released

--- console.py
+++ console.py
@@ -16,19 +16,23 @@ from models.review import Review
 def parse(arg):
     curly_braces = re.search(r"\{(.*?)\}", arg)
     brackets = re.search(r"\[(.*?)\]", arg)
-    if curly_brackets is None:
-        if brackets is None:
-            return [i.strip(",") for i in split(arg)]
-        else:
-            lexer = split(arg[:brackets.span()[0]])
-            retl = [i.strip(",") for i in lexer]
-            retl.append(brackets.group())
-            return retl
-    else:
+
+    if curly_braces is None and brackets is None:
+        return [i.strip(",") for i in split(arg)]
+
+    retl = []
+
+    if curly_braces is not None:
         lexer = split(arg[:curly_braces.span()[0]])
-        retl = [i.strip(",") for i in lexer]
+        retl.extend([i.strip(",") for i in lexer])
         retl.append(curly_braces.group())
-        return retl
+
+    if brackets is not None:
+        lexer = split(arg[:brackets.span()[0]])
+        retl.extend([i.strip(",") for i in lexer])
+        retl.append(brackets.group())
+
+    return retl
 
 
 class HBNBCommand(cmd.Cmd):
